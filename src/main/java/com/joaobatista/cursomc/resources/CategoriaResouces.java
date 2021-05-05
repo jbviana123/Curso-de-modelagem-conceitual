7c58333aaba49cf03520c2fcb5edb01e553f6a11
joao batista viana <joaobvianna@hotmail.com>
2021-05-05 13:56:07 -0300

    Testando  a primeira classe de dominio categoria

--- src/main/java/com/joaobatista/cursomc/resources/CategoriaResouces.java
+++ src/main/java/com/joaobatista/cursomc/resources/CategoriaResouces.java
@@ -1,15 +1,33 @@
 package com.joaobatista.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.joaobatista.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouces {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando !!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		
+		
+		
 	}
 
 }
